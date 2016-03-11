fce9e7fa2b2f3b6abd9faaf8445e9b974d310263
Atul Nambudiri <Atul-Nambudiri@github.com>
2016-03-11 11:38:51 -0600

    Extra stuff

--- STMPi/bluetoothserver.py
+++ STMPi/bluetoothserver.py
@@ -1,4 +1,4 @@
-import bluetooth
+import bluetooth #pybluez
 import time
 import random
 import threading
@@ -7,7 +7,7 @@ import serial
 serv_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)		#Create a bluetooth socket
 uuid = "37407000-8cf0-11bd-b23e-10b75c30d20a"
 
-ser = serial.Serial('/dev/ttyACM6', 9600) #The serial port the arduino is connected on. Change this if needed
+ser = serial.Serial('/dev/ttyACM0', 9600) #The serial port the arduino is connected on. Change this if needed
 
 print("Creating Bluetooth Server");
 
@@ -24,7 +24,7 @@ sending = False
 def send_pictures():		#This method will send all 11 images in a row, if scanning has started.
 	global i
 	global sending
-	if sending != False:
+	if sending == True:
 		images = ["scan_images/0.jpg", "scan_images/1.jpg", "scan_images/2.jpg", "scan_images/3.jpg", "scan_images/4.jpg", "scan_images/5.jpg", "scan_images/6.jpg", "scan_images/7.jpg", "scan_images/8.jpg", "scan_images/9.jpg", "scan_images/10.jpg"]
 		item = images[i]
 		with open(item, "rb") as image:
@@ -41,7 +41,7 @@ def send_pictures():		#This method will send all 11 images in a row, if scanning
 			i = 0
 			sending = False
 		else:
-			threading.Timer(10.0, send_pictures).start()
+			threading.Timer(8.0, send_pictures).start()
 
 while(True):		#This waits and reads for incoming commands.
 	command = socket.recv(100)
